feee0ec86d38d7fdaf0258f7d9c68f90db58fa48
shrutpatelscu <s.patel.30@student.scu.edu.au>
2021-05-29 01:51:08 +0800

    Parser.java
    Modified comments for parser class

--- Assignment-3 Shrut Patel/MyGame2/MyGame2/src/main/java/ass3/mygame2/Parser.java
+++ Assignment-3 Shrut Patel/MyGame2/MyGame2/src/main/java/ass3/mygame2/Parser.java
@@ -3,20 +3,11 @@ package ass3.mygame2;
 import java.util.Scanner;
 
 /**
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- * 
- * This parser reads user input and tries to interpret it as an "Adventure"
- * command. Every time it is called it reads a line from the terminal and
- * tries to interpret the line as a two word command. It returns the command
- * as an object of class Command.
  *
- * The parser has a set of known command words. It checks user input against
- * the known commands, and if the input is not one of the known commands, it
- * returns a command object that is marked as an unknown command.
  * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ Parser class is the class that takes the input from the user and then work on it.
+ * @author  Shrut Patel
+ * @version 29-05-2021
  */
 public class Parser 
 {
@@ -37,7 +28,7 @@ public class Parser
      */
     public Command getCommand() 
     {
-        String inputLine;   // will hold the full input line
+        String inputLine;  
         String word1 = null;
         String word2 = null;
 
@@ -55,8 +46,7 @@ public class Parser
             }
         }
 
-        // Now check whether this word is known. If so, create a command
-        // with it. If not, create a "null" command (for unknown command).
+       
         if(commands.isCommand(word1)) {
             return new Command(word1, word2);
         }
