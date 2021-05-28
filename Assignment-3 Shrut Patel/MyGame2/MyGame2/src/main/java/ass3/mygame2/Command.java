d1358032fea63ad70e6a08f80b6a9468a5dbc958
shrutpatelscu <s.patel.30@student.scu.edu.au>
2021-05-29 01:17:51 +0800

    Command.Java
    Deleted example and extra comments

--- Assignment-3 Shrut Patel/MyGame2/MyGame2/src/main/java/ass3/mygame2/Command.java
+++ Assignment-3 Shrut Patel/MyGame2/MyGame2/src/main/java/ass3/mygame2/Command.java
@@ -4,8 +4,7 @@
  *
  * This class holds information about a command that was issued by the user.
  * A command currently consists of two strings: a command word and a second
- * word (for example, if the command was "take map", then the two strings
- * obviously are "take" and "map").
+ * word 
  * 
  * The way this is used is: Commands are already checked for being valid
  * command words. If the user entered an invalid command (a word that is not
@@ -13,7 +12,7 @@
  *
  * If the command had only one word, then the second word is <null>.
  * 
- * @author  Michael Kölling and David J. Barnes
+ * @author Shrut Patel
  * @version 2016.02.29
  */
 
@@ -25,8 +24,7 @@ public class Command
     private String secondWord;
 
     /**
-     * Create a command object. First and second word must be supplied, but
-     * either one (or both) can be null.
+     * 
      * @param firstWord The first word of the command. Null if the command
      *                  was not recognised.
      * @param secondWord The second word of the command.
