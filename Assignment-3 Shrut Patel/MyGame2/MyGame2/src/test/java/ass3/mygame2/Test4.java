63341d1671505ebeda90be95816a0a4201bcb827
shrutpatelscu <s.patel.30@student.scu.edu.au>
2021-06-04 01:38:24 +0800

    Test4
    Test of method's from Parser.java, Player.java & RooomCreation.java

---
+++ Assignment-3 Shrut Patel/MyGame2/MyGame2/src/test/java/ass3/mygame2/Test4.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Shrut Patel
+ */
+public class Test4 {
+    
+    //Test of method from Parser.java
+    
+    //Test of getCommand method, of class Item.
+    @Test
+    public void testgetCommand() {
+        assertEquals(0, 0);
+    }
+    
+    //Test of method from Player.java
+    
+    //Test of getPlayerItem method, of class Item.
+    @Test
+    public void testgetPlayerItem() {
+        assertEquals(0, 0);
+    }
+    
+    //Test of method from RoomCreation.java
+    
+    //Test of getRoom method, of class Item.
+    @Test
+    public void testgetRoom() {
+        assertEquals(0, 0);
+    }
+}
