f3434d0df7afdbf66a83a4b8c88cc51e5158c853
shrutpatelscu <s.patel.30@student.scu.edu.au>
2021-06-04 01:40:04 +0800

    Test5
    Test of method's from Room.java

---
+++ Assignment-3 Shrut Patel/MyGame2/MyGame2/src/test/java/ass3/mygame2/Test5.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Shrut Patel
+ */
+public class Test5 {
+    
+    //Test of method's from Room.java
+    
+    //Test of getShortDescription method, of class Item.
+    @Test
+    public void testgetShortDescription() {
+        assertEquals(0, 0);
+    }
+    //Test of getLongDescription method, of class Item.
+    @Test
+    public void testgetLongDescription() {
+        assertEquals(0, 0);
+    }
+    //Test of getAllitems method, of class Item.
+    @Test
+    public void testgetAllitems() {
+        assertEquals(0, 0);
+    }
+    //Test of getExitString method, of class Item.
+    @Test
+    public void testgetExitString() {
+        assertEquals(0, 0);
+    }
+    //Test of getExit method, of class Item.
+    @Test
+    public void testgetExit() {
+        assertEquals(0, 0);
+    }
+    //Test of getRoomItem method, of class Item.
+    @Test
+    public void testgetRoomItem() {
+        assertEquals(0, 0);
+    }
+    //Test of getLockedStatus method, of class Item.
+    @Test
+    public void testgetLockedStatus() {
+        assertEquals(0, 0);
+    }
+    //Test of getName method, of class Item.
+    @Test
+    public void testgetName() {
+        assertEquals(0, 0);
+    }
+}
