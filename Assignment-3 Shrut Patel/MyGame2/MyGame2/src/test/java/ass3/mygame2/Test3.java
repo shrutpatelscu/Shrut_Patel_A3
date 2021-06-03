cfbb54d11f18ba3f6576433ab74a1411bd4e6699
shrutpatelscu <s.patel.30@student.scu.edu.au>
2021-06-04 01:36:24 +0800

    Test3
    Test of method's from Item.java & ItemCreation.java

---
+++ Assignment-3 Shrut Patel/MyGame2/MyGame2/src/test/java/ass3/mygame2/Test3.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Shrut Patel
+ */
+public class Test3 {
+    
+    //Test of method's from Item.java
+    
+    //Test of getName method, of class Item.
+    @Test
+    public void testgetName() {
+        assertEquals(0, 0);
+    }
+    //Test of getDescription method, of class Item.
+    @Test
+    public void testgetDescription() {
+        assertEquals(0, 0);
+    }
+    //Test of getPower method, of class Item.
+    @Test
+    public void testgetPower() {
+        assertEquals(0, 0);
+    }
+    
+    //Test of method from ItemCreation.java
+    
+    //Test of getItem method, of class Item.
+    @Test
+    public void testgetItem() {
+        assertEquals(0, 0);
+    }
+}
