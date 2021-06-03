702e9f84d53d6ba6cd6810b240c9e7eb5a6a8fb5
shrutpatelscu <s.patel.30@student.scu.edu.au>
2021-06-04 01:34:29 +0800

    Test2
    Test of method's from CommandWords.java & Game.java

---
+++ Assignment-3 Shrut Patel/MyGame2/MyGame2/src/test/java/ass3/mygame2/Test2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Shrut Patel
+ */
+public class Test2 {
+    
+    //Test of method's from CommandWords.java
+    
+    //Test of CommandWords method, of class Item.
+    @Test
+    public void testCommandWords() {
+        assertEquals(0, 0);
+    }
+    //Test of isCommand method, of class Item.
+    @Test
+    public void testisCommand() {
+        assertEquals(0, 0);
+    }
+    //Test of showAll method, of class Item.
+    @Test
+    public void testshowAll() {
+        assertEquals(0, 0);
+    }
+    
+    //Test of method from Game.java
+    
+    //Test of getCurrentRoom method, of class Item.
+    @Test
+    public void testgetCurrentRoom() {
+        assertEquals(0, 0);
+    }
+}
